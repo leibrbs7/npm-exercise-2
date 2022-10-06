ff160a34f6f07beb8481e7c5691ef55911dcbcae
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-06 10:25:35 +0800

    fixed double quotes to single quotes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const process = require('process');
 
 let args = process.argv.slice(2);
-console.log("number of arguments: ", args.length);
+console.log('number of arguments: ', args.length);
 
 let finalString  = args.join(" ");
 
 const countVowels = str => Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
-console.log("number of vowels: ", countVowels(finalString));
+console.log('number of vowels: ', countVowels(finalString));
