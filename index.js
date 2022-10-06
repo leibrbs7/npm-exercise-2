ebc4bf03667029d5bf52f94cc91294cccc9f6deb
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-06 10:17:40 +0800

    process.argv

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const process = require('process');
+
+let args = process.argv.slice(2);
+console.log("number of arguments: ", args.length);
+
+let finalString  = args.join(" ");
+
+const countVowels = str => Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
+console.log("number of vowels: ", countVowels(finalString));
