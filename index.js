e86fa93cba937481b90da5be575842119c12267d
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-11 08:47:17 +0800

    formatted with prettier and added prettierrc file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const process = require('process');
 let args = process.argv.slice(2);
 console.log('number of arguments: ', args.length);
 
-let finalString  = args.join(" ");
+let finalString = args.join(' ');
 
-const countVowels = str => Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
+const countVowels = (str) => Array.from(str).filter((letter) => 'aeiou'.includes(letter)).length;
 console.log('number of vowels: ', countVowels(finalString));
